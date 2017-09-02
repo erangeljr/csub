b8251abe95eadacce2ac76d4963d160ad0f59cc0
Eddie Rangel <hacker@eddierangel.com>
2017-09-01 21:20:08 -0700

    update content

--- cmps2240-f17/code/lab1/hello.s
+++ cmps2240-f17/code/lab1/hello.s
@@ -1,21 +1,37 @@
-# hello.s
-# a sample MIPS program to demonstrate MIPS basics 
-# Usage: $ spim -f hello.s
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 12
+	.globl	_main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$1, %edi
+	leaq	L_.str(%rip), %rsi
+	movl	$6, %edx
+	movl	$0, -4(%rbp)
+	movl	$5, -8(%rbp)
+	movb	$0, %al
+	callq	_write
+	xorl	%edx, %edx
+	movl	%eax, -12(%rbp)         ## 4-byte Spill
+	movl	%edx, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
 
-        .data                # data segment begins here
-stuff:  .asciiz "Hello World!\n"
-
-        .text                # code segment begins here 
-main:
-        la $a0, stuff
-        li $v0, 4            # 4 is syscall to print a string
-        syscall              # execute the call 
-
-        li $a0, 10           # 10 is ascii value for linefeed
-        li $v0, 11           # 11 is syscall to print char
-        syscall
-
-        li $v0, 10           # 10 is system call to exit
-        syscall              # execute the call 
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"hello\n"
 
 
+.subsections_via_symbols
